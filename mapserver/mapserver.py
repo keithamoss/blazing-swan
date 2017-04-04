e862c931ddd0704a59fa4e73039e2b3c985efc75
Keith Moss <keith.moss@landgate.wa.gov.au>
2017-04-04 19:30:51 +0800

    We now make snapshots every 10 minutes

--- mapserver/mapserver.py
+++ mapserver/mapserver.py
@@ -1,24 +1,29 @@
 from pyspatialite import dbapi2 as db
 from subprocess import call
+from shutil import copyfile
 import os
 import random
 import time
 
 # CONFIG START
 sleepTime = 3 # seconds - time to sleep after generating a map image
+snapshotInterval = 10 # minutes - how often snapshots are saved to ./snapshots
 # CONFIG END
 
-sqlite_path = "./data/bikes.sqlite"
+sqlitePath = "./data/bikes.sqlite"
+snapshotCounter = 0
 
-if not os.path.isfile(sqlite_path):
-    print "Error: Database %s doesn't exist." % (sqlite_path)
+if not os.path.isfile(sqlitePath):
+    print "Error: Database %s doesn't exist." % (sqlitePath)
 
 else:
     # Connect
-    # conn = db.connect(sqlite_path)
+    # conn = db.connect(sqlitePath)
     # cur = conn.cursor()
 
     while True:
+        snapshotCounter += 1
+
         if os.path.isfile("./bikes.tmp.png"):
             os.remove("./bikes.tmp.png")
         
@@ -35,6 +40,18 @@ else:
         # if os.path.isfile("./bikes.png"):
         #     os.remove("./bikes.png")
         # os.rename("./bikes.tmp.png", "./bikes.png")
+
+        if (snapshotCounter * sleepTime) >= (snapshotInterval * 60):
+            print "Snapshot!"
+            snapshotCounter = 0
+
+            copyfile("./bikes.png", "./snapshots/map-%s.png" % (time.strftime("%Y-%m-%d-%H-%M-%S")))
+
+            if os.path.isfile("./snapshots/map-latest.png"):
+                os.remove("./snapshots/map-latest.png")
+            copyfile("./bikes.png", "./snapshots/map-latest.png")
+        else:
+            print "Snapshot pending! (%s vs %s)" % ((snapshotCounter * sleepTime), (snapshotInterval * 60))
 
         print "Map generated."
         time.sleep(sleepTime)
